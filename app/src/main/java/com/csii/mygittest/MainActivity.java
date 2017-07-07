6b0b7402c1702ed44e12b8d61e63533498063d7b
zhanghu <1015484306@qq.com>
2017-07-07 11:17:56 +0800

    新建分支

--- app/src/main/java/com/csii/mygittest/MainActivity.java
+++ app/src/main/java/com/csii/mygittest/MainActivity.java
@@ -3,7 +3,23 @@ package com.csii.mygittest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static String Ip = null;
+
+    static {
+        //Ip = "https://jlmcweb.chinabond.com.cn/Product_pweb/";
+        //Ip = "http://10.1.1.15:8090/paygate/weixin";
+        //Ip = "http://10.1.1.194:8090/paygate/weixin";
+        Ip = "https://wxhj.csii.com.cn/paygate/weixin";//生产地址
+        //Ip = "http://115.182.90.201/paygate/weixin";
+        //Ip = "http://devwx.csii.com.cn/paygate/weixin";//测试环境
+    }
+
+    public static JSONObject userInfo = new JSONObject();
+    public static JSONObject loginInfo;//存储登陆信息（这个好用）
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
